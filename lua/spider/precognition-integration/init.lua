0b084100cda46fc2ff0c99eb169433dfa9751199
Diogo Tandeta Tartarotti <tandetat@grinnell.edu>
2025-04-02 17:33:04 -0300

    fix: attemp to fix README linting issue, change precognition integration to register adapter

--- lua/spider/precognition-integration/init.lua
+++ lua/spider/precognition-integration/init.lua
@@ -1,15 +1,14 @@
---- Exposes Precognition Spider Motions Adapter
----@diagnostic disable-next-line: undefined-doc-name
----@return Precognition.MotionsAdapter
-function init()
+local M = {}
+--- Register Precognition Spider Motions Adapter
+M.register_adapter = function()
 	local globalOpts = require("spider.config").globalOpts
 	local ok, vanillaMotions = pcall(require, "precognition.motions.vanilla_motions")
 	if not ok then
 		vim.api.nvim_echo({ { "[Spider] `precognition` not found" } }, true, { err = false })
-		return {}
+		return
 	end
 	local spider = require("spider.motion-logic")
-	return {
+	local adapter = {
 		next_word_boundary = function(str, cursorcol, linelen, bigWord)
 			if bigWord then return vanillaMotions.next_word_boundary(str, cursorcol, linelen, bigWord) end
 			return spider.getNextPosition(str, cursorcol, "w", globalOpts) or 0
@@ -24,10 +23,7 @@ function init()
 		end,
 		-- precognition.nvim currently does not support multi character motions (see https://github.com/tris203/precognition.nvim/issues/101#issuecomment-2676676721)
 	}
+	require("precognition.motions").register_motions(adapter)
 end
--- --- Exposes Precognition Spider Motions Adapter
--- ---@return Precognition.MotionsAdapter
--- function M.expose_adapter()
--- end
-local M = init()
+
 return M
